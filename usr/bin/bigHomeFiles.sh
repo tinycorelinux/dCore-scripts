b6fe66abf7b1509f243f3e1135f040f2a362492a
Robert Shingledecker <rshingledecker@gmail.com>
2013-10-18 14:58:16 -0700

    bigHomeFiles.sh: Busybox relocate adjustments.

--- usr/bin/bigHomeFiles.sh
+++ usr/bin/bigHomeFiles.sh
@@ -1,4 +1,7 @@
-#!/bin/busybox ash
+#!/bb/ash
 . /etc/init.d/tc-functions
-useBusybox
+
+PATH="/bb:/bin:/sbin:/usr/bin:/usr/sbin"
+export PATH
+
 find ${HOME} -type f -size +1024k | xargs ls -lSh 2>/dev/null |  awk '{printf "%s\t%s\n",$5,$9}'
