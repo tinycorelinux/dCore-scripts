fde1a4eed6d57c52a09e51fe65db82a1b4fb99ec
Robert Shingledecker <rshingledecker@gmail.com>
2013-10-18 15:11:30 -0700

    exitcheck.sh: Busybox relocate adjustments.

--- usr/bin/exitcheck.sh
+++ usr/bin/exitcheck.sh
@@ -1,10 +1,11 @@
-#!/bin/busybox ash
+#!/bb/ash
 # Check for optional local shutdown before actual shutdown/reboot.
 # Called from exittc
 # (c) Robert Shingledecker 2006-2010
 . /etc/init.d/tc-functions
-useBusybox
 
+PATH="/bb:/bin:/sbin:/usr/bin:/usr/sbin"
+export PATH
 
 [ -x /opt/shutdown.sh ] && /opt/shutdown.sh
 
