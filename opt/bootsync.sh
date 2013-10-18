6dab6592cef8883333079a524746ebf8f3878513
Robert Shingledecker <rshingledecker@gmail.com>
2013-10-18 14:50:09 -0700

    bootsync.sh: Busybox relocate adjustments.

--- opt/bootsync.sh
+++ opt/bootsync.sh
@@ -1,7 +1,6 @@
-#!/bin/sh
+#!/bb/ash
 # put other system startup commands here, the boot process will wait until they complete.
 # Use bootlocal.sh for system startup commands that can run in the background 
 # and therefore not slow down the boot process.
-echo performance > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 /usr/bin/sethostname box
 /opt/bootlocal.sh &
