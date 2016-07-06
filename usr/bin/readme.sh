f2bb07dce39301ee0f895a39005a990ac6678199
Jason Williams <jason5876@gmail.com>
2016-07-06 00:19:27 +0000

    Improvements the readme.sh

--- usr/bin/readme.sh
+++ usr/bin/readme.sh
@@ -5,14 +5,30 @@ PATH="/bb:/bin:/sbin:/usr/bin:/usr/sbin"
 export PATH
 BUILD=`getBuild`
 MIRROR=`cat /opt/tcemirror`
-VER=`getMajorVer`
+README="$1"
+if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+	echo " "
+	echo "${YELLOW}readme.sh - View README files for dCore packages.${NORMAL}"
+	echo " "
+	echo "Usage:"
+	echo " "
+	echo "${YELLOW}"readme.sh gcc"${NORMAL}             View the gcc readme file."
+	echo "${YELLOW}"readme.sh"${NORMAL}             View available readme files."
+	echo " "
+fi
 
-[ -f /tmp/README-"$1".txt ] && rm /tmp/README-"$1".txt
-if wget -s "$MIRROR"/"$VER".x/"$BUILD"/README/README-"$1".txt > /dev/null 2>&1; then
-   wget -q "$MIRROR"/"$VER".x/"$BUILD"/README/README-"$1".txt -O /tmp/README-"$1".txt 
-   more /tmp/README-"$1".txt
+if [ -z "$1" ]; then
+	wget -q "$MIRROR"/dCore/"$BUILD"/README/READMELIST -O /tmp/READMELIST
+	select2 "Select SCE to load." /tmp/READMELIST	
+	README="$(cat  /tmp/select.ans)"
+fi
+
+[ -f /tmp/README-"$README".txt ] && rm /tmp/README-"$README".txt
+if wget -s "$MIRROR"/dCore/"$BUILD"/README/README-"$README".txt > /dev/null 2>&1; then
+   wget -q "$MIRROR"/dCore/"$BUILD"/README/README-"$README".txt -O /tmp/README-"$README".txt 
+   more /tmp/README-"$README".txt
    exit 1
 else 
-   echo ""$1" does not have a README file, exiting..."
+   echo ""$README" does not have a README file, exiting..."
 fi
 
