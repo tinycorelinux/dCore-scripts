7f79103e4a9b085fb00855caa5a16d5b9cad12f1
Jason Williams <jason5876@gmail.com>
2016-09-18 14:57:29 +0000

    Cosmetic and help menu updates and more streamlined output.

--- usr/bin/readme.sh
+++ usr/bin/readme.sh
@@ -10,22 +10,22 @@ checknotroot
 [ -f /tmp/select.ans ] && sudo rm /tmp/select.ans
 
 if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
-	clear
-	echo "${YELLOW}readme.sh - View essential dCore README files to set up a dCore system${NORMAL}, includes"
-	echo "            how to set up a TCE directory, graphics, sound, wireless, gcc build"
-	echo "            essential compile tools, full featured LibreOffice, numerous Window"
-	echo "            Managers/Desktop Environments and more."
 	echo " "
-	echo "            Internet connection needed to access READMEs through the readme.sh"
-	echo "            command. To view READMEs, Enter key scrolls down one line, spacebar"
-	echo "            pages down, (q)uit exits. READMEs downloaded to /tmp."
+	echo "${YELLOW}readme.sh - View essential dCore README files to set up a dCore system${NORMAL}."
+	echo "            Specific information to set up a TCE directory, graphics, sound,"
+	echo "            wireless, gcc build essential compile tools, Window Managers,"
+	echo "            Desktop Environments and specific software packages."
+	echo " "
+	echo "            Internet connection needed to access READMEs. Viewed READMEs"
+	echo "            downloaded to /tmp/. To view READMEs use the Enter, spacebar,"
+	echo "            Up/Down arrow and/or Page Up/Down keys, (q)uit exits."
 	echo " "
 	echo "            READMEs located at http://tinycorelinux.net/dCore/x86/README/."
 	echo " "
 	echo "Usage:"
 	echo " "
 	echo "${YELLOW}"readme.sh NAME"${NORMAL}    View specific README, example 'readme.sh 1st'."
-	echo "${YELLOW}"readme.sh"${NORMAL}         Menu prompt, select README to view from all available."
+	echo "${YELLOW}"readme.sh"${NORMAL}         Menu prompt, select README from list."
 	echo " "
 	exit 0
 fi
@@ -41,16 +41,24 @@ else
 	exit_tcnet
 fi
 
+selectList () {
+[ -f /tmp/select.ans ] && sudo rm /tmp/select.ans
 if [ -z "$1" ]; then
 	wget -q "$MIRROR"/dCore/"$BUILD"/README/READMELIST -O /tmp/READMELIST
-	select2 "Select README file." /tmp/READMELIST	
+	select2 "Select README. Use Page or Arrow Up/Down keys to read, (q)uit exits README." /tmp/READMELIST	
 	README="$(cat  /tmp/select.ans)"
+	[ -f /tmp/README-"$README".txt ] && rm /tmp/README-"$README".txt
+	if wget -s "$MIRROR"/dCore/"$BUILD"/README/README-"$README".txt > /dev/null 2>&1; then
+		wget -q "$MIRROR"/dCore/"$BUILD"/README/README-"$README".txt -O /tmp/README-"$README".txt
+		clear
+		less /tmp/README-"$README".txt
+		selectList
+	else
+		echo "No selection made, exiting.."
+		exit 0
+	fi
 fi
-
-if [ -z "$README" ]; then
-	echo "No selection made, exiting.."
-	exit 0
-fi
+}
 
 [ -f /tmp/README-"$README".txt ] && rm /tmp/README-"$README".txt
 if wget -s "$MIRROR"/dCore/"$BUILD"/README/README-"$README".txt > /dev/null 2>&1; then
@@ -58,7 +66,8 @@ if wget -s "$MIRROR"/dCore/"$BUILD"/README/README-"$README".txt > /dev/null 2>&1
    clear
    more /tmp/README-"$README".txt
    exit 1
-else 
-   echo ""$README" does not have a README file, exiting..."
+elif [ -z "$1" ]; then
+	selectList
+else
+   echo "'"$README"' does not have a README file, exiting..."
 fi
-
